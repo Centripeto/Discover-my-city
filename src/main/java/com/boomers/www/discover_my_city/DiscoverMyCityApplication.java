51dd164fa2dbeaf232b09bfae1701ae6827a78e1
Mattia Izzi <izzimattia@yahoo.it>
2023-12-07 21:32:02 +0100

    first commit

---
+++ src/main/java/com/boomers/www/discover_my_city/DiscoverMyCityApplication.java
@@ -0,0 +1,13 @@
+package com.boomers.www.discover_my_city;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoverMyCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoverMyCityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boomers/www/discover_my_city/DiscoverMyCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boomers.www.discover_my_city;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverMyCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
